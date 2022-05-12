79610896356b5dbb4b23692600c1c8da08cdd28a
Chris Kjolhede <ckjolhede@gmail.com>
2022-05-12 12:11:52 -0600

    write validation and relationship tests

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -3,4 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Airline, type: :model do
+    describe 'validations' do
+        it { should validate}
+    end
 end
--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -3,4 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
+    describe 'validations' do
+        it { should validate_presence_of :flight_number}
+        it { should validate_presence_of :destination}
+        it { should validate_presence_of :nonstop }
+    end
+    
+    describe 'relationship' do
+        it { should belong_to :airline }
+    end
+
+
 end
