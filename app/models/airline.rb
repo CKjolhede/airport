ae3e28cff0d9fb57f7baad19383debf94d7e9357
Chris Kjolhede <ckjolhede@gmail.com>
2022-05-14 20:09:50 -0600

    validate airline model params

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,9 @@
 class Airline < ApplicationRecord
   has_many :Flight
+
+  validate_presence_of :name
+  validate_presence_of :delays
+  validate_presence_of :rating
+  
+
 end
--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -4,6 +4,12 @@ require 'rails_helper'
 
 RSpec.describe Airline, type: :model do
     describe 'validations' do
-        it { should validate}
+        it { should validate_presence_of :name}
+        it { should validate_presence_of :delays}
+        it { should validate_presence_of :rating}
+    end
+
+    describe 'relationships' do
+        it {should have_mamy :flights}
     end
 end
