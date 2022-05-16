4ce239a8d0cd88e344ba787c43d3fed908fcf00c
Chris Kjolhede <ckjolhede@gmail.com>
2022-05-15 19:01:25 -0600

    create airlineflights controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   get '/airlines', to: 'airlines#index'
   get '/airlines/:id', to: 'airlines#show'
+  get '/airlines/:airline_id/flights', to: 'airline_flights#index'
 
   get '/flights', to: 'flights#index'
   get '/flights/:id', to: 'flights#show'
