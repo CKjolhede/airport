1630cb17c623bbf1782d76edadaa81e3884674f3
Chris Kjolhede <ckjolhede@gmail.com>
2022-05-16 07:46:45 -0600

    write model method #flights_count

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -22,4 +22,16 @@ RSpec.describe Airline, type: :model do
       expect(Airline.sort_by_creation).to eq([airline2, airline1, airline4, airline3])
     end
   end
+
+  describe 'instance methods' do
+    it 'can count the number of flights associated with an airline' do
+      airline1 = Airline.create(name: 'Alpha Air Lines', on_time: 'true', rating: 2)
+      flight11 = airline1.flights.create(destination: 'Miami', flight_number: 11, nonstop: true)
+      flight12 = airline1.flights.create(destination: 'New York', flight_number: 12, nonstop: false)
+      flight13 = airline1.flights.create(destination: 'Atlanta', flight_number: 13, nonstop: true)
+      flight14 = airline1.flights.create(destination: 'Washington DC', flight_number: 14, nonstop: false)
+
+      expect(airline1.flights_count).to eq(4)
+    end
+  end
 end
