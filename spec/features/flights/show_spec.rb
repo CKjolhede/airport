c15516042c3c665fed73abd058e9801fa0ea3a72
Chris Kjolhede <ckjolhede@gmail.com>
2022-05-18 17:10:30 -0600

    write TEST for flights show delete

--- spec/features/flights/show_spec.rb
+++ spec/features/flights/show_spec.rb
@@ -26,4 +26,20 @@ RSpec.describe Flight, type: :feature do
       expect(current_path).to eq("/flights/#{@flight11.id}/edit")
     end
   end
+
+  context "delete button" do
+    it 'removes the record' do
+      airline1 = Airline.create(name: 'Alpha Air Lines', rating: 2)
+        flight11 = airline1.flights.create(destination: 'Miami', flight_number: 11, nonstop: true)
+        flight12 = airline1.flights.create(destination: 'New York', flight_number: 12, nonstop: false)
+
+        visit "flights/#{flight11.id}"
+
+        click_on "Delete Flight"
+
+      expect(current_path).to eq("/flights")
+      expect(page).to_not have_content("Miami")
+      expect(page).to have_content("New York")
+    end
+  end
 end
